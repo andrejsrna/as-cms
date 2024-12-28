1711f52a13735fda25e42c1dba9ba9fff52a59af
Andrej Srna <andrejsrna@MacBook-Pro-uzivatela-Andrej.local>
2024-12-28 19:54:45 +0100

    slug, featured

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -15,6 +15,36 @@ export const Posts: CollectionConfig = {
       required: true,
     },
     {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      admin: {
+        position: 'sidebar',
+      },
+      hooks: {
+        beforeValidate: [
+          ({ value, data = {} }) => {
+            if (!value && data?.title) {
+              return data.title
+                .toLowerCase()
+                .replace(/[^a-z0-9]+/g, '-')
+                .replace(/^-|-$/g, '')
+            }
+            return value
+          },
+        ],
+      },
+    },
+    {
+      name: 'featuredImage',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+      admin: {
+        position: 'sidebar',
+      },
+    },
+    {
       name: 'content',
       type: 'richText',
       required: true,
